7f4c5280b3cbe117a118617203e0d4e0d91f8f73
bjellesma <william.jellesma@gmail.com>
2020-06-12 01:14:31 -0400

    init commit

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+from secure import CONNECT_STRING, APP_DEBUG
+SECRET_KEY = os.urandom(32)
+# Grabs the folder where the script runs.
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Enable debug mode.
+DEBUG = APP_DEBUG
+
+# Connect to the database
+
+
+# TODO IMPLEMENT DATABASE URL
+SQLALCHEMY_DATABASE_URI = CONNECT_STRING
+SQLALCHEMY_TRACK_MODIFICATIONS=False
